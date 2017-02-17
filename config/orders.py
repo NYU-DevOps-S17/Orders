9a26253f534e85644e6fd60b1b59b2a53fa9452c
MatthewChiappa <mchiappa@albany.edu>
2017-02-16 21:57:47 -0500

    building a folder structure for the project

---
+++ config/orders.py
@@ -0,0 +1,6 @@
+######################################################################
+
+# THIS FILE WILL BE USED TO STORE DICTIONARIES OF THE ORDERS UNTIL WE 
+# DECIDE TO MIGRATE THIS TO A WORKING DATABASE
+
+######################################################################
