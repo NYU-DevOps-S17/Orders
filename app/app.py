5a84cf745c29de2e5cc46997357c88cb5a6f5f07
MatthewChiappa <mchiappa@albany.edu>
2017-02-20 14:47:37 -0500

    fixing the rest api to work with vagrant

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-# Copyright 2016 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2017 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,67 +13,141 @@
 # limitations under the License.
 
 import os
-from redis import Redis
-from flask import Flask, jsonify, request
+from threading import Lock
+from flask import Flask, Response, jsonify, request, json
+from orders import *
 
 # Create Flask application
 app = Flask(__name__)
 
-# Get bindings from the environment
-debug = (os.getenv('DEBUG', 'False') == 'True')
-port = os.getenv('PORT', '5000')
-hostname = os.getenv('HOSTNAME', '127.0.0.1')
-redis_port = os.getenv('REDIS_PORT', '6379')
+# Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_409_CONFLICT = 409
 
-# Application Routes
+# Lock for thread-safe counter increment
+lock = Lock()
 
 ######################################################################
-# GET /
+# GET INDEX
 ######################################################################
 @app.route('/')
 def index():
-    orders_url = request.base_url + 'orders'
-    return jsonify(name='Orders Resource', version='1.0', url=orders_url), 200
+    orders_url = request.base_url + "orders"
+    return jsonify(name='Orders Demo REST API Service',
+                   version='1.0',
+                   url=orders_url
+                   ), HTTP_200_OK
 
 ######################################################################
-# GET /orders?foo=bar
+# LIST ALL Orders
 ######################################################################
-# @app.route('/orders')
-# def list_orders():
-#     return jsonify(response='Not Implemented Yet.'), 200
+@app.route('/orders', methods=['GET'])
+def list_orders():
+    results = orders.values()
+    amount_paid = request.args.get('amount_paid')
+    if amount_paid:
+        results = []
+        for key, value in orders.iteritems():
+            if value['amount_paid'] == amount_paid:
+                results.append(orders[key])
+
+    return reply(results, HTTP_200_OK)
 
 ######################################################################
-# GET /orders/:id
+# RETRIEVE A Order
 ######################################################################
-# @app.route('/orders')
-# def get_order():
-#     return jsonify(response='Not Implemented Yet.'), 200
+@app.route('/orders/<int:id>', methods=['GET'])
+def get_orders(id):
+    if orders.has_key(id):
+        message = orders[id]
+        rc = HTTP_200_OK
+    else:
+        message = { 'error' : 'Order with id: %s was not found' % str(id) }
+        rc = HTTP_404_NOT_FOUND
 
-######################################################################		
-# POST /orders/:id
-######################################################################
-# @app.route('/orders')
-# def create_order():
-#     return jsonify(response='Not Implemented Yet.'), 201
+    return reply(message, rc)
 
-######################################################################		
-# PUT /orders/:id
 ######################################################################
-# @app.route('/orders')
-# def update_order():
-#     return jsonify(response='Not Implemented Yet.'), 204
+# ADD A NEW Order
+######################################################################
+@app.route('/orders', methods=['POST'])
+def create_orders():
+    payload = request.get_json()
+    if is_valid(payload):
+        id = next_index()
+        orders[id] = {'id': id, 'customer_name': payload['customer_name'], 'amount_paid': payload['amount_paid']}
+        message = orders[id]
+        rc = HTTP_201_CREATED
+    else:
+        message = { 'error' : 'Data is not valid' }
+        rc = HTTP_400_BAD_REQUEST
 
-######################################################################		
-# DELETE /orders/:id
+    return reply(message, rc)
+
 ######################################################################
-# @app.route('/orders')
-# def delete_order():
-#     return jsonify(response='Not Implemented Yet.'), 200
+# UPDATE AN EXISTING Order
+######################################################################
+@app.route('/orders/<int:id>', methods=['PUT'])
+def update_orders(id):
+    if orders.has_key(id):
+        payload = request.get_json()
+        if is_valid(payload):
+            orders[id] = {'id': id, 'customer_name': payload['customer_name'], 'amount_paid': payload['amount_paid']}
+            message = orders[id]
+            rc = HTTP_200_OK
+        else:
+            message = { 'error' : 'Order data was not valid' }
+            rc = HTTP_400_BAD_REQUEST
+    else:
+        message = { 'error' : 'Order %s was not found' % id }
+        rc = HTTP_404_NOT_FOUND
+
+    return reply(message, rc)
+
+######################################################################
+# DELETE A Order
+######################################################################
+@app.route('/orders/<int:id>', methods=['DELETE'])
+def delete_orders(id):
+    del orders[id];
+    return '', HTTP_204_NO_CONTENT
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+def next_index():
+    global current_order_id
+    with lock:
+        current_order_id += 1
+    return current_order_id
+
+def reply(message, rc):
+    response = Response(json.dumps(message))
+    response.headers['Content-Type'] = 'application/json'
+    response.status_code = rc
+    return response
+
+def is_valid(data):
+    valid = False
+    try:
+        customer_name = data['customer_name']
+        amount_paid = data['amount_paid']
+        valid = True
+    except KeyError as err:
+        app.logger.error('Missing parameter error: %s', err)
+    return valid
 
 
 ######################################################################
 #   M A I N
 ######################################################################
 if __name__ == "__main__":
-    redis = Redis(host=hostname, port=int(redis_port))
+    current_order_id = 2
+    # Pull options from environment
+    debug = (os.getenv('DEBUG', 'False') == 'True')
+    port = os.getenv('PORT', '5000')
     app.run(host='0.0.0.0', port=int(port), debug=debug)
---
+++ app/orders.py
@@ -0,0 +1,11 @@
+######################################################################
+
+# THIS FILE WILL BE USED TO STORE DICTIONARIES OF THE ORDERS UNTIL WE 
+# DECIDE TO MIGRATE THIS TO A WORKING DATABASE
+
+######################################################################
+
+orders = { 
+	1: {'id': 1, 'customer_name': 'Tom', 'amount_paid': 200}, 
+	2: {'id': 2, 'customer_name': 'Bob', 'amount_paid': 300} 
+}
